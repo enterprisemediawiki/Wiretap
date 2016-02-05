e7ba5022c0253e594462e8911dafbc0e5ca0ceb8
James Montalvo <edwin.j.montalvo@nasa.gov>
2016-02-05 15:39:33 -0500

    begin wfLoadExtension prep

--- Wiretap.php
+++ Wiretap.php
@@ -1,5 +1,18 @@
 <?php
 
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'Wiretap' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['Wiretap'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['WiretapAlias'] = __DIR__ . '/Wiretap.alias.php';
+	wfWarn(
+		'Deprecated PHP entry point used for Wiretap extension. Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+}
+// else, use the old method of extension registry...
+
 $wgExtensionCredits['specialpage'][] = array(
 	'path' => __FILE__,
 	'name' => 'Wiretap',
