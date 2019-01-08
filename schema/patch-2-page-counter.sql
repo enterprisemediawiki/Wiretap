2ee7299ab2d0a51502e1a1a49377a4fd6cd25010
Greg Rundlett <greg@freephile.com>
2019-01-08 07:07:55 -0500

    Fix Index Conflict
    When upgrading Wiretap, you will get an error when it tries
    to create the unique indexex that already exist. So, we
    delete the indexex prior to creating them in the SQL files.

--- schema/patch-2-page-counter.sql
+++ schema/patch-2-page-counter.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS /*_*/wiretap_counter_period (
 	count          INT(8) UNSIGNED NOT NULL DEFAULT 0,
 	count_unique   INT(8) UNSIGNED NOT NULL DEFAULT 0
 ) /*$wgDBTableOptions*/;
+DROP INDEX /*i*/wiretap_counter_period_page_id ON /*_*/wiretap_counter_period;
 CREATE UNIQUE INDEX /*i*/wiretap_counter_period_page_id ON /*_*/wiretap_counter_period (page_id);
 
 CREATE TABLE IF NOT EXISTS /*_*/wiretap_counter_alltime (
@@ -12,4 +13,5 @@ CREATE TABLE IF NOT EXISTS /*_*/wiretap_counter_alltime (
 	count          INT(8) UNSIGNED NOT NULL DEFAULT 0,
 	count_unique   INT(8) UNSIGNED NOT NULL DEFAULT 0
 );
+DROP INDEX /*i*/wiretap_counter_alltime_page_id ON /*_*/wiretap_counter_alltime;
 CREATE UNIQUE INDEX /*i*/wiretap_counter_alltime_page_id ON /*_*/wiretap_counter_alltime (page_id);
